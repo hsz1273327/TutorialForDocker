2c79267a88580054050f1562d2b954dfea5ceef0
HUANG SIZHE <hsz1273327@gmail.com>
2020-12-08 18:33:37 +0800

    update

--- build_manifest.ps1
+++
@@ -1,3 +0,0 @@
-docker manifest create hsz1273327/hellodocker:0.0.0 hsz1273327/hellodocker:armv6-0.0.0 hsz1273327/hellodocker:armv7-0.0.0 hsz1273327/hellodocker:arm64-0.0.0 hsz1273327/hellodocker:amd64-0.0.0
-
-docker manifest create hsz1273327/hellodocker:latest hsz1273327/hellodocker:armv6-latest hsz1273327/hellodocker:armv7-latest hsz1273327/hellodocker:arm64-latest hsz1273327/hellodocker:amd64-latest
