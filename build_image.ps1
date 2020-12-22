7f50f75ae5ea76e6c535c183f722c5944025456f
hsz1273327 <hsz1273327@gmail.com>
2020-12-23 02:02:03 +0800

    update

--- build_image.ps1
+++ build_image.ps1
@@ -1,5 +1,5 @@
 
-docker buildx build --load --platform=linux/arm/v6 -t dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0 -t dev.hszofficial.site:9443/test/hellodocker:armv6-latest .
-docker buildx build --load --platform=linux/arm/v7 -t dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0 -t dev.hszofficial.site:9443/test/hellodocker:armv7-latest .
-docker buildx build --load --platform=linux/arm64 -t dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0 -t dev.hszofficial.site:9443/test/hellodocker:arm64-latest .
-docker buildx build --load --platform=linux/amd64 -t dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0 -t dev.hszofficial.site:9443/test/hellodocker:amd64-latest .
+docker buildx build --load --platform=linux/arm/v6 -t dev.hszofficial.site:9443/stock/hellodocker:armv6-0.0.0 -t dev.hszofficial.site:9443/stock/hellodocker:armv6-latest .
+docker buildx build --load --platform=linux/arm/v7 -t dev.hszofficial.site:9443/stock/hellodocker:armv7-0.0.0 -t dev.hszofficial.site:9443/stock/hellodocker:armv7-latest .
+docker buildx build --load --platform=linux/arm64 -t dev.hszofficial.site:9443/stock/hellodocker:arm64-0.0.0 -t dev.hszofficial.site:9443/stock/hellodocker:arm64-latest .
+docker buildx build --load --platform=linux/amd64 -t dev.hszofficial.site:9443/stock/hellodocker:amd64-0.0.0 -t dev.hszofficial.site:9443/stock/hellodocker:amd64-latest .
--- build_manifest.sh
+++ build_manifest.sh
@@ -1,13 +1,14 @@
-export DOCKER_CONTENT_TRUST=1
-export DOCKER_CONTENT_TRUST_SERVER="https://dev.hszofficial.site:4443"
-docker push dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:armv6-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:armv7-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:arm64-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:amd64-latest
+# export DOCKER_CONTENT_TRUST=1
+# export DOCKER_CONTENT_TRUST_SERVER="https://dev.hszofficial.site:4443"
 
-docker manifest create dev.hszofficial.site:9443/test/hellodocker:0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0 dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0 dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0
-docker manifest create dev.hszofficial.site:9443/test/hellodocker:latest dev.hszofficial.site:9443/test/hellodocker:armv6-latest dev.hszofficial.site:9443/test/hellodocker:armv7-latest dev.hszofficial.site:9443/test/hellodocker:arm64-latest dev.hszofficial.site:9443/test/hellodocker:amd64-latest
+docker push dev.hszofficial.site:9443/stock/hellodocker:armv6-0.0.0
+docker push dev.hszofficial.site:9443/stock/hellodocker:armv7-0.0.0
+docker push dev.hszofficial.site:9443/stock/hellodocker:arm64-0.0.0
+docker push dev.hszofficial.site:9443/stock/hellodocker:amd64-0.0.0
+docker push dev.hszofficial.site:9443/stock/hellodocker:armv6-latest
+docker push dev.hszofficial.site:9443/stock/hellodocker:armv7-latest
+docker push dev.hszofficial.site:9443/stock/hellodocker:arm64-latest
+docker push dev.hszofficial.site:9443/stock/hellodocker:amd64-latest
+
+# docker manifest create dev.hszofficial.site:9443/test/hellodocker:0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0 dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0 dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0
+# docker manifest create dev.hszofficial.site:9443/test/hellodocker:latest dev.hszofficial.site:9443/test/hellodocker:armv6-latest dev.hszofficial.site:9443/test/hellodocker:armv7-latest dev.hszofficial.site:9443/test/hellodocker:arm64-latest dev.hszofficial.site:9443/test/hellodocker:amd64-latest
