e41d40f8885165639c65a9514a4e9b55dc0c51d6
hsz1273327 <hsz1273327@gmail.com>
2021-01-04 00:48:42 +0800

    update

--- build_manifest.ps1
+++ build_manifest.ps1
@@ -1,14 +1,14 @@
 $env:DOCKER_CONTENT_TRUST=1
 $env:DOCKER_CONTENT_TRUST_SERVER="https://dev.hszofficial.site:4443"
 
-docker push dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0
-docker push dev.hszofficial.site:9443/test/hellodocker:armv6-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:armv7-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:arm64-latest
-docker push dev.hszofficial.site:9443/test/hellodocker:amd64-latest
+docker push dev.hszofficial.site:9443/test/hello-docker:armv6-0.0.0
+docker push dev.hszofficial.site:9443/test/hello-docker:armv7-0.0.0
+docker push dev.hszofficial.site:9443/test/hello-docker:arm64-0.0.0
+docker push dev.hszofficial.site:9443/test/hello-docker:amd64-0.0.0
+docker push dev.hszofficial.site:9443/test/hello-docker:armv6-latest
+docker push dev.hszofficial.site:9443/test/hello-docker:armv7-latest
+docker push dev.hszofficial.site:9443/test/hello-docker:arm64-latest
+docker push dev.hszofficial.site:9443/test/hello-docker:amd64-latest
 
-# docker manifest create dev.hszofficial.site:9443/test/hellodocker:0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv6-0.0.0 dev.hszofficial.site:9443/test/hellodocker:armv7-0.0.0 dev.hszofficial.site:9443/test/hellodocker:arm64-0.0.0 dev.hszofficial.site:9443/test/hellodocker:amd64-0.0.0
-# docker manifest create dev.hszofficial.site:9443/test/hellodocker:latest dev.hszofficial.site:9443/test/hellodocker:armv6-latest dev.hszofficial.site:9443/test/hellodocker:armv7-latest dev.hszofficial.site:9443/test/hellodocker:arm64-latest dev.hszofficial.site:9443/test/hellodocker:amd64-latest
+# docker manifest create dev.hszofficial.site:9443/test/hello-docker:0.0.0 dev.hszofficial.site:9443/test/hello-docker:armv6-0.0.0 dev.hszofficial.site:9443/test/hello-docker:armv7-0.0.0 dev.hszofficial.site:9443/test/hello-docker:arm64-0.0.0 dev.hszofficial.site:9443/test/hello-docker:amd64-0.0.0
+# docker manifest create dev.hszofficial.site:9443/test/hello-docker:latest dev.hszofficial.site:9443/test/hello-docker:armv6-latest dev.hszofficial.site:9443/test/hello-docker:armv7-latest dev.hszofficial.site:9443/test/hello-docker:arm64-latest dev.hszofficial.site:9443/test/hello-docker:amd64-latest
